8c844d330ec7d4731f1c56f90dfecd3908dd4056
shafikul-islam <179398ssshofik@gmail.com>
2024-01-10 21:54:51 +0600

    fix credential problem

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -18,44 +18,44 @@ const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // CredentialsProvider({
-    //   name: "credentials",
-    //   credentials: {
-    //     email: { label: "email", type: "text" },
-    //     password: { label: "password", type: "password" },
-    //   },
-    //   async authorize(credentials) {
-    //     if (!credentials?.email || !credentials?.password) {
-    //       throw new Error("Invalid credentials");
-    //     }
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
 
-    //     const user = await prisma.user.findUnique({
-    //       where: {
-    //         email: credentials.email,
-    //       },
-    //     });
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-    //     if (!user || !user?.hashedPassword) {
-    //       throw new Error("Invalid credentials");
-    //     }
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
 
-    //     const isCorrectPassword = await bcrypt.compare(
-    //       credentials.password,
-    //       user.hashedPassword
-    //     );
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
 
-    //     if (!isCorrectPassword) {
-    //       throw new Error("Invalid credentials");
-    //     }
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
 
-    //     return user;
-    //   },
-    // }),
+        return user;
+      },
+    }),
   ],
-  // debug: process.env.NODE_ENV === "development",
-  // session: {
-  //   strategy: "jwt",
-  // },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
